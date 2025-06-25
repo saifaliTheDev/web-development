1cb8d8b1949932119147483ba0c1a4b7ee15863e
SAIF ALI RABBANI <saifu100@outlook.com>
2025-06-25 09:02:55 +0500

    Initial commit

---
+++ React/my-app/src/pages/Frontend/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = ()=>{
+    return (  
+    <div className="text-center m-4">
+    <h1 className="text-6xl mb-8 ">Learning React</h1>
+    <hr />
+    <h2 className="text-4xl mt-8 ">First Class..</h2>
+    </div>
+    )
+  
+}
+
+export default Home;
